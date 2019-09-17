8b4b90b0ffd7f42fd679f5fe09feaf453393ef9e
Syarif Hidayat <srf.hidayat@gmail.com>
2019-09-17 10:06:42 +0700

    modified token in login.

--- src/component/Login.js
+++ src/component/Login.js
@@ -53,9 +53,9 @@ class LoginComponent extends Component {
         console.log("Password: " + this.state.formControls.password.value);
 
         ProxyServices.executeBasicAuthenticationService(this.state.formControls.username.value, this.state.formControls.password.value)
-            .then(response => response)
+            .then(response => response.data)
             .then((json) => {
-                console.log("TOKEN:", JSON.stringify(json.data.token));
+                console.log("TOKEN FROM SERVER:", JSON.stringify(json.data.token));
                 console.log("Response:", JSON.stringify(json));
                 ProxyServices.registerSuccessfulLogin(this.state.formControls.username.value, this.state.formControls.password.value, json.data.token);
                 if(this.props.location.state){
